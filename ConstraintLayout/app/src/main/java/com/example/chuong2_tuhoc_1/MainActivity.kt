1942b351b71568c071998354142200966b2e47e6
pthienquan1 <quan01011997123@gmail.com>
2023-09-02 21:45:45 +0700

    Calculate tip app

--- ConstraintLayout/app/src/main/java/com/example/chuong2_tuhoc_1/MainActivity.kt
+++ ConstraintLayout/app/src/main/java/com/example/chuong2_tuhoc_1/MainActivity.kt
@@ -2,36 +2,34 @@ package com.example.chuong2_tuhoc_1
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.dice_roll)
+        setContentView(R.layout.tip_time)
 
-        val rollButton: Button = findViewById(R.id.btn1)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
-        rollDice()
+//        val rollButton: Button = findViewById(R.id.btn1)
+//        rollButton.setOnClickListener {
+//            rollDice()
+//        }
+//        rollDice()
     }
-
-    private fun rollDice() {
-        val diceRoll = Dice(6).rollDice();
-        val diceImage: ImageView = findViewById(R.id.imageView);
-        val drawableResource = when (diceRoll) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-
-        diceImage.setImageResource(drawableResource)
-        diceImage.contentDescription = diceRoll.toString()
-    }
+//
+//    private fun rollDice() {
+//        val diceRoll = Dice(6).rollDice();
+//        val diceImage: ImageView = findViewById(R.id.imageView);
+//        val drawableResource = when (diceRoll) {
+//            1 -> R.drawable.dice_1
+//            2 -> R.drawable.dice_2
+//            3 -> R.drawable.dice_3
+//            4 -> R.drawable.dice_4
+//            5 -> R.drawable.dice_5
+//            else -> R.drawable.dice_6
+//        }
+//
+//        diceImage.setImageResource(drawableResource)
+//        diceImage.contentDescription = diceRoll.toString()
+//    }
 }
